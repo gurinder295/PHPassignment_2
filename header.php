cbf653c258b43bda0ff82248d8412a71d06fa76c
gurinder295 <200622633@student.georgianc.on.ca>
2025-04-04 09:45:30 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="books.php">Manage Books</a></li>
+            <?php if (isset($_SESSION['user_id'])): ?>
+                <li><a href="logout.php">Logout (<?= $_SESSION['username']; ?>)</a></li>
+            <?php else: ?>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="login.php">Login</a></li>
+            <?php endif; ?>
+        </ul>
+    </nav>
+</header>
