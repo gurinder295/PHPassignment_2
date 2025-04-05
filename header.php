898945b5c4d76f73243a91618c07f4d83802d325
gurinder295 <200622633@student.georgianc.on.ca>
2025-04-04 23:02:16 -0400

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@ session_start();
 <header>
     <nav>
         <ul>
-            <li><a href="index.php">Home</a></li>
+            <li><a href="index.html">Home</a></li>
             <li><a href="books.php">Manage Books</a></li>
             <?php if (isset($_SESSION['user_id'])): ?>
                 <li><a href="logout.php">Logout (<?= $_SESSION['username']; ?>)</a></li>
