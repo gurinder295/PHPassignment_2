0eb3cd898a270301d4388c7682aeb9ec54033262
gurinder295 <200622633@student.georgianc.on.ca>
2025-04-04 22:52:45 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,26 @@
 
+<?php
+include 'header.php';
+require 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->execute([$username]);
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['username'] = $user['username'];
+        header("Location: index.php");
+    } else {
+        echo "Invalid login credentials.";
+    }
+}
+?>
+<form method="post">
+    <label>Username: <input type="text" name="username"></label><br>
+    <label>Password: <input type="password" name="password"></label><br>
+    <input type="submit" value="Login">
+</form>
